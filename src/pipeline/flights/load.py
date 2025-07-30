ab915f4c21bb471a2896ac1205627f41aac75e3c
deezinn <developer.andrelz@gmail.com>
2025-07-29 21:43:46 -0300

    feat: new load to 2 dataframe (airline, flights) p2

--- src/pipeline/flights/load.py
+++ src/pipeline/flights/load.py
@@ -3,7 +3,8 @@ from .extract import FlightsHttp
 
 def save_in_csv():
    air = FlightsHttp().getFlights('https://api.schiphol.nl/public-flights/flights?sort=%2BscheduleTime')
-   dataframe = pd.DataFrame(air['airlines'])
+
+   dataframe = pd.DataFrame(air['flights'])
    dataframe.to_csv('flights.csv', index_label='index')
    return dataframe
 
