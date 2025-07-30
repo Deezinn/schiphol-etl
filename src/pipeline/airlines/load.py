780bda490546fda150925c7b7e88ccb6339c8af1
deezinn <developer.andrelz@gmail.com>
2025-07-30 20:44:32 -0300

    feat: new estruture to pipeline

--- src/pipeline/airlines/load.py
+++ src/pipeline/airlines/load.py
@@ -4,7 +4,7 @@ from .extract import AirlinesHttp
 def save_in_csv():
    air = AirlinesHttp().getAirlines('https://api.schiphol.nl/public-flights/airlines?sort=%2Biata')
    dataframe = pd.DataFrame(air['airlines'])
-   dataframe.to_csv('airlines.csv', index_label='index')
+   dataframe.to_csv('src/data/airlines.csv', index_label='index')
    return dataframe
 
 save_in_csv()
--- src/pipeline/flights/load.py
+++ src/pipeline/flights/load.py
@@ -3,9 +3,8 @@ from .extract import FlightsHttp
 
 def save_in_csv():
    air = FlightsHttp().getFlights('https://api.schiphol.nl/public-flights/flights?sort=%2BscheduleTime')
-
    dataframe = pd.DataFrame(air['flights'])
-   dataframe.to_csv('flights.csv', index_label='index')
+   dataframe.to_csv('src/data/flights.csv', index_label='index')
    return dataframe
 
 save_in_csv()
