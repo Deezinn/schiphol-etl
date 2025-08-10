ecd02ca3b10280925240dd04ebb8200c6d755b50
deezinn <developer.andrelz@gmail.com>
2025-08-10 17:24:20 -0300

    feat: new loadtypes

--- src/pipeline/common/load.py
+++ src/pipeline/common/load.py
@@ -3,12 +3,12 @@
 def save_in_csv(dataframe, tag):
   match tag:
       case "aircraftTypes":
-        dataframe.to_csv(f"src/data/{tag}.csv")
+        dataframe.to_csv(f"src/data/raw/{tag}.csv")
       case "airlines":
-        dataframe.to_csv(f"src/data/{tag}.csv")
+        dataframe.to_csv(f"src/data/raw/{tag}.csv")
       case "destinations":
-        dataframe.to_csv(f"src/data/{tag}.csv")
+        dataframe.to_csv(f"src/data/raw/{tag}.csv")
       case "flights":
-        dataframe.to_csv(f"src/data/{tag}.csv")
+        dataframe.to_csv(f"src/data/raw/{tag}.csv")
       case _:
         print("Tag invalida!")
