08917a85716e3bb5487eef55b674086860400685
deezinn <developer.andrelz@gmail.com>
2025-08-15 22:45:21 -0300

    feat: new database connection

---
+++ src/infrastructure/local_database/sqlite_execute.py
@@ -0,0 +1,19 @@
+import sqlite3
+import os
+class SqliteDatabase:
+  def __init__(self, db_path):
+    self.db_path = db_path
+    if self.db_path:
+      os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
+
+  def execute_query(self, query = None):
+    try:
+      with sqlite3.connect(self.db_path) as connection:
+        print("Conexão iniciada")
+        cursor = connection.cursor()
+        print("Iniciando query")
+        cursor.execute(query)
+        print("Conexão finalizada")
+        connection.commit()
+    except sqlite3.Error as e:
+      raise f"Erro{e}"
